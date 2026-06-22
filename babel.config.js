2cdfb11c3cf907a1fc1790b3eee4ba7ca0537b6c
tushcmd <muturidavid854@gmail.com>
2026-06-23 02:54:15 +0300

    fix: remove invalid app.json fields and babel plugin

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'nativewind/babel',
       // reanimated MUST be last
       'react-native-reanimated/plugin',
     ],
