776223b32d08f232d4af3a3d82d15f014e1b2382
tushcmd <muturidavid854@gmail.com>
2026-06-23 13:14:11 +0300

    refactor: styling

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,9 @@
 ﻿module.exports = function (api) {
   api.cache(true);
-  const isDev = process.env.NODE_ENV !== 'production' && !process.env.CI;
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ...(isDev ? ['nativewind/babel'] : []),
+      'nativewind/babel',
       // reanimated MUST be last
       'react-native-reanimated/plugin',
     ],
