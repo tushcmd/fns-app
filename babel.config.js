0d4fdd0b110cff18e6a3e008a91f7377fc93c2aa
tushcmd <muturidavid854@gmail.com>
2026-06-23 03:57:04 +0300

    fix: enable nativewind babel conditionally for dev builds

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,12 @@
-module.exports = function (api) {
+﻿module.exports = function (api) {
   api.cache(true);
+  const isDev = process.env.NODE_ENV !== 'production' && !process.env.CI;
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      ...(isDev ? ['nativewind/babel'] : []),
       // reanimated MUST be last
       'react-native-reanimated/plugin',
     ],
   };
 };
-// module.exports = function (api) {
-//   api.cache(true);
-//   let plugins = [];
-
-//   plugins.push('react-native-worklets/plugin');
-
-//   return {
-//     presets: ['babel-preset-expo'],
-//     plugins: ['react-native-reanimated/plugin'],
-//   };
-// };
